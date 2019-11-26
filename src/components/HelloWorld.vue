b77b780b76d60d2ef2827b3fce6a9a7e691bf2c1
HayaUp <hayaup.git@gmail.com>
2019-11-26 22:03:32 +0900

    [add]Vue.draggableを使ってみる

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,81 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+    <div>
+      <label for="">この数値以上のリストアイテムを表示する</label>
+      <input type="number" v-model="inputNumber">
+    </div>
+
+    <div>
+      <draggable v-model="list" v-on:update="onUpdate">
+        <div
+          v-for="(item, index) in list" v-bind:key="index"
+          v-bind:style="listItemStyle"
+          v-show="item.level >= inputNumber">
+          {{item.name}}
+        </div>
+      </draggable>
+    </div>
+
+    <div>以下、別のコンポーネント</div>
+
+    <div>
+      <draggable v-model="list" v-on:update="onUpdate">
+        <ListItem
+          v-for="(item, index) in list" v-bind:key="index"
+          v-bind:style="listItemStyle"
+          v-show="item.level >= inputNumber"
+          v-bind:item="item">
+        </ListItem>
+      </draggable>
+    </div>
   </div>
 </template>
 
 <script>
+//
+import draggable from 'vuedraggable'
+import ListItem from '@/components/ListItem.vue'
+
 export default {
   name: 'HelloWorld',
-  data () {
+  components : {
+    draggable,
+    ListItem
+  },
+  data : function() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      list : [
+        {name : "sample1", level : 1},
+        {name : "sample2", level : 2},
+        {name : "sample3", level : 3},
+        {name : "sample4", level : 4},
+        {name : "sample5", level : 5},
+        {name : "sample6", level : 6},
+        {name : "sample7", level : 7},
+        {name : "sample8", level : 8},
+        {name : "sample9", level : 9},
+        {name : "sample10", level : 10},
+      ],
+      listItemStyle : {
+        margin : "auto",
+        width : "10%",
+        border : "1px solid black"
+      },
+      inputNumber : 0
+    }
+  },
+  watch : {
+    list : function(new_value, old_value) {
+      console.log("old", old_value);
+      console.log("new", new_value);
+    }
+  },
+  methods : {
+    onUpdate : function() {
+      console.log("Update");
     }
   }
 }
